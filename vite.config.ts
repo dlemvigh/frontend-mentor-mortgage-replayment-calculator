d40b0b2954ee75ad7c33e8ada59cc55f8e6118ab
David Lemvigh <dlemvigh@gmail.com>
2025-02-03 21:14:57 +0100

    Move base url to environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/frontend-mentor-mortgage-replayment-calculator/',
+  base: process.env.VITE_BASE_URL || "/",
   plugins: [react()],
   css: {
     modules: {
